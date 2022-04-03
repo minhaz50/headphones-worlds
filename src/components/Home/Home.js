637fa90f99aa3cb2f03d522e40a0c5addbbcf5f8
minhaz2597 <md.minhazuddin2597@gmail.com>
2022-04-04 01:28:09 +0600

    create componetn folder

---
+++ src/components/Home/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="text-img-conatiner">
+        <div className="text-container">
+          <h1 className="headphone-title">Grab Your Favourite HEADPHONES🎧</h1>
+          <p>
+            Are you looking for a wireless earbud-type headset <br /> that
+            delivers an impressive playback time? If your answer is yes, bring
+            home the <br /> <strong>Beats By Dre</strong> <br /> that comes with
+            a carrying case. This carrying case also acts as a 650 mAh portable
+            charger that extends the playback time of these earbuds by up to 15
+            hours
+          </p>
+          <button className="liveDemo-btn">Live Demo</button>
+        </div>
+        <div className="image-container">
+          <img src="./Headphones-Transparent-PNG.png" alt="" />
+        </div>
+      </div>
+      <div className="comments-container">
+        <h1>Customer Review</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Notfound/Notfound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Notfound = () => {
+  return (
+    <div>
+      <h3> Not Found</h3>
+      <p>404</p>
+    </div>
+  );
+};
+
+export default Notfound;
