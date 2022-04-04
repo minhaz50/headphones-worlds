4cee4f6a909d2a3dc3dc67b6f4176aeedd69b079
minhaz2597 <md.minhazuddin2597@gmail.com>
2022-04-05 02:36:30 +0600

    code modified again

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>Want to know more about us please contact.</h1>
+    <div className="about">
+      <h1 className="about-title">
+        Want to know more about us please contact.
+      </h1>
+      <img
+        src="https://images.pexels.com/photos/10335320/pexels-photo-10335320.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
     </div>
   );
 };
--- src/components/Notfound/Notfound.js
+++ src/components/Notfound/Notfound.js
@@ -3,7 +3,7 @@ import "./Notfound.css";
 
 const Notfound = () => {
   return (
-    <div>
+    <div className="not-found">
       <h3 className="title">Page Not Found</h3>
       <img
         src="https://cdn.pixabay.com/photo/2016/04/24/13/24/error-1349562_960_720.png"
